7e8ca20fd9cced06e98f0b24518811b45c7515f3
Ubuntu <ubuntu@ip-172-31-8-33.us-west-1.compute.internal>
2017-09-25 22:27:59 +0000

    modified mp3towav to convert all mp3 songs in a directory to wav. Updated remote server with Data/classical_piano_mp3s and Data/classical_piano_wavs, which can be used for the training set.

--- Data/mp3towav.py
+++ Data/mp3towav.py
@@ -1,7 +1,26 @@
 from pydub import AudioSegment
 import sys
-if len(sys.argv) != 0:
-    print("error, needs exactly 1 argument (the mp3 location")
-else:
-    sound = AudioSegment.from_mp3(sys.argv[0])
-    sound.export("/output/path", format="wav")
+import os
+
+if len(sys.argv) != 2:
+    print("error, needs exactly 1 argument (the absolute path to mp3 directory")
+    os.exit()
+
+directory_in_str = sys.argv[1]
+directory = os.fsencode(directory_in_str)
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".mp3"): 
+        try:
+            sound = AudioSegment.from_mp3(os.path.join(directory_in_str,filename))
+            # sound = AudioSegment.from_mp3(sys.argv[0])
+            sound.export(directory_in_str+"wav/"+filename+".wav", format="wav")
+            log = "converted from mp3 to wav and saved: "+str(filename)
+        except:
+            log = "something went wrong. Maybe bad mp3? "+str(filename)
+            
+        with open("mp3towav.py.log", "a") as logfile:
+            logfile.write(log)
+        print(log)
+
